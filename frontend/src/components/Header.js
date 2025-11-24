e193e24fadb39ee0bce86ea9a285f2b9c8c13c12
Aryakl7 <lalwani.aryak12@gmail.com>
2025-11-24 21:26:35 +0530

    Updated search bar width and border styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -61,12 +61,13 @@ const Header = () => {
                 </Link>
             </div>
 
-            <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
-                <input type='text' placeholder='search product here...' className='w-full outline-none' onChange={handleSearch} value={search}/>
-                <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
-                  <GrSearch />
-                </div>
-            </div>
+            {/* MODIFIED CODE */}
+          <div className='hidden lg:flex items-center w-full justify-between max-w-md border-2 border-red-200 rounded-full focus-within:shadow pl-2'>
+              <input type='text' placeholder='search product here...' className='w-full outline-none' onChange={handleSearch} value={search}/>
+              <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
+                <GrSearch />
+              </div>
+          </div>
 
 
             <div className='flex items-center gap-7'>
